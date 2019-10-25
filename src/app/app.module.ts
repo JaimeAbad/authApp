30d41b5fd0b87557984884d57a52b13ee2acfc9b
Jaime <aquileseeb@gmail.com>
2019-10-25 09:16:55 +0200

    c1
    diapo18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProtegidaComponent } from './components/protegida/protegida.component';
+import { PreciosComponent } from './components/precios/precios.component';
+import { CallbackComponent } from './components/callback/callback.component';
+import { AuthService } from './services/auth.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    ProtegidaComponent,
+    PreciosComponent,
+    CallbackComponent,
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/routes/routes.component.ts
@@ -0,0 +1,24 @@
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from '../components/home/home.component';
+import { PreciosComponent } from '../components/precios/precios.component';
+import { ProtegidaComponent } from '../components/protegida/protegida.component';
+import { CallbackComponent } from '../components/callback/callback.component';
+
+const APP_ROUTES: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'precios', component: PreciosComponent },
+  { path: 'protegida', component: ProtegidaComponent, canActivate: [] },
+  { path: 'callback', component: CallbackComponent },
+  { path: '**', pathMatch: 'full', redirectTo: 'home' }
+];
+
+import { NgModule } from '@angular/core';
+
+@NgModule({
+  imports: [RouterModule.forRoot(APP_ROUTES)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+
+
+export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES);
